c15865abf6c361d3cea99ed376286af6d9bd51ce
Nabysayna <nabysayna@gmail.com>
2017-04-13 16:48:17 +0000

    set up .travis.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works. Continuous integration run successfully as well as Deployment on Firebase. So, what else ?';
 }
